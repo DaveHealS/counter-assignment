52a9fd92ec74104dc681b76db7bf828ebc6b5e2c
DaveHealS <114851206+DaveHealS@users.noreply.github.com>
2024-11-13 15:05:00 -0500

    individual render now before each test
    Shae requested the beforeEach-method to not be used with the render-call. it is not allowed in the react testing library. instead, each test calls render indiviually.

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -3,25 +3,23 @@ import { render, screen, fireEvent } from "@testing-library/react";
 // import the Counter component
 import Counter from "../components/Counter";
 
-beforeEach(() => {
-    // Render the Counter component
-    render(<Counter />);
-});
-
 test('renders counter message', () => {
     // check rendering
+    render(<Counter />);
     const counterMessage = screen.getByText(/counter/i);
     expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
     // check inital state
+    render(<Counter />);
     const countElement = screen.getByTestId("count");
     expect(countElement).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
     // Test + button
+    render(<Counter />);
     const incrementButton = screen.getByText("+");
     fireEvent.click(incrementButton);
     const countElement = screen.getByTestId("count");
@@ -30,6 +28,7 @@ test('clicking + increments the count', () => {
 
 test('clicking - decrements the count', () => {
     // Test - button
+    render(<Counter />);
     const decrementButton = screen.getByText("-");
     fireEvent.click(decrementButton);
     const countElement = screen.getByTestId("count");
